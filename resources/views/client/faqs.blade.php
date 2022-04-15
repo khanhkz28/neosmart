32aaa300cd7be31a4f9a0fd8314f7df411e5fe9c
cuibapn2014 <56261497+cuibapn2014@users.noreply.github.com>
2022-04-15 09:02:27 +0700

    Update menu and contact page

--- resources/views/client/faqs.blade.php
+++ resources/views/client/faqs.blade.php
@@ -4,15 +4,13 @@
 @section('breadcrumb-second', 'Hỏi đáp')
 @section('main')
 <main>
-  <div id="banner-faq d-flex flex-row">
-    <div class="caption col-lg-6">
-      <h2 class="display-4 text-left fst-italic">FAQ</h2>
+  <div id="banner-faq" class="d-flex flex-row-reserve justify-content-between container-xxl p-5">
+    <div class="caption col-lg-5">
+      <h2 class="display-4 text-left fst-italic">FAQs</h2>
       <p class="text">Hệ thống nhà thông minh thế hệ mới nhất trên thế giới, công nghệ vượt trội so với các sản phẩm của
         Mỹ và Châu Âu - theo tiêu chí xếp hạng của Garther</p>
     </div>
-    <div class="col-lg-6">
-      <img src="/images/client/Hfaq.png" alt="Image Faq" />
-    </div>
+    <img class="col-lg-5" src="/images/client/Hfaq.png" alt="Image Faq" />
   </div>
 </main>
 @endsection
